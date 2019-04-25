6c94e6029bc9032b13a13184989a8d89c1f6d141
Milind Technopreneur <milindtechnopreneur@Milinds-MacBook-Air.local>
2019-04-25 06:59:49 +0530

    commit

--- src/main/java/com/example/demo/ActuatorConfig.java
+++ src/main/java/com/example/demo/ActuatorConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-//@Configuration
+//@Configurations
 public class ActuatorConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
